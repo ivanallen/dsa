c87fd885902b80fd80ceadcf0a7174b64fb0f9e3
allen <1007729991@qq.com>
2019-04-19 16:54:22 +0800

    fix

--- heap/heap.h
+++ heap/heap.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <functional>
 
-template <typename T, typename U = std::less<T>>
+template <typename T, typename Compare = std::less<T>>
 class Heap {
 public:
     void sink(int i) {
@@ -14,15 +14,16 @@ public:
             int j = 2*i + 1;
             if (j >= size) return;
 
-            if (j < size - 1 && _u(_tree[j], _tree[j+1])) ++j;
-            if (_u(_tree[i], _tree[j]))
+            if (j < size - 1 && _cmp(_tree[j], _tree[j+1])) ++j;
+            if (_cmp(_tree[i], _tree[j]))
                 std::swap(_tree[i], _tree[j]);
             i = j;
         }
     }
 
-    void insert(T&& val) {
-        _tree.push_back(std::forward<T>(val));
+    template <typename U>
+    void insert(U&& val) {
+        _tree.push_back(std::forward<U>(val));
         for (int i = _tree.size() - 1; i >= 0; --i) {
             sink(i);
         }
@@ -50,6 +51,6 @@ public:
         sink(0);
     }
 private:
-    U _u;
+    Compare _cmp;
     std::vector<T> _tree;
 };
