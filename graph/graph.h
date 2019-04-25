2c68b57310ed078631656e8fe7aa13da6b1f6fb6
allen <1007729991@qq.com>
2019-04-25 18:04:34 +0800

    graph

--- graph/graph.h
+++ graph/graph.h
@@ -1,7 +1,88 @@
 #pragma once
 #include <map>
 #include <queue>
+#include <set>
 
+
+template <typename T>
+class Graph {
+public:
+    class Vertex;
+    // 暂时只能添加，因此 size 是递增的，可以充当 id
+    template <typename U>
+    int add_vertex(U&& value) {
+        Vertex v(std::forward<U>(value), _size++);
+        int id = v.id();
+        _adjcent.emplace(id, std::set<int>{});
+        _vertexes.emplace(id, std::move(v));
+        return id;
+    }
+
+    bool add_edge(int u, int v) {
+        auto a = _vertexes.find(u);
+        if (a == _vertexes.end()) return false;
+        auto b = _vertexes.find(v);
+        if (b == _vertexes.end()) return false;
+        _adjcent[u].insert(v);;
+        return true;
+    }
+
+    T& value(int id) {
+        auto it = _vertexes.find(id);
+        if (it != _vertexes.end()) {
+            return it->second.value();
+        }
+        throw std::runtime_error("no such vertex `" + std::to_string(id) + "`");
+    }
+
+    template <typename FUNC>
+    void for_each(FUNC&& cb) {
+        for (auto& pair : _vertexes) {
+            cb(pair.first, pair.second.value(), _adjcent[pair.first]);
+        }
+    }
+
+    void BFS() {
+    }
+private:
+
+private:
+    std::map<int, Vertex> _vertexes;
+    std::map<int, std::set<int>> _adjcent;
+    int _size = 0;
+};
+
+template <typename T>
+class Graph<T>::Vertex {
+public:
+    friend class Graph;
+    enum Color {
+        WHITE = 0,
+        GRAY,
+        BLACK
+    };
+
+    T& value() {
+        return *_value;
+    }
+private:
+    template <typename U>
+    Vertex(U&& value, int id) : _id(id) {
+        _value.reset(new T(std::forward<U>(value)));
+    }
+
+    int id() const {
+        return _id;
+    }
+private:
+    std::unique_ptr<T> _value;
+    int _id = 0;
+    Vertex* _parent = nullptr;
+    int _depth = 0;
+    Color _color = Color::WHITE;
+};
+
+/*
 class Graph {
 public:
     void add_edge(int a, int b) {
@@ -40,3 +121,4 @@ private:
     std::map<int, int> _v;
     std::map<int, std::vector<int>> _adj;
 };
+*/
--- graph/graph_test.cpp
+++ graph/graph_test.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <vector>
 #include "graph.h"
 
-int main() {
+/*
+int test() {
     Graph g;
     g.add_edge(5, 2);
     g.add_edge(5, 0);
@@ -19,4 +21,46 @@ int main() {
         std::cout << e << " ";
     }
     std::cout << std::endl;
+}
+*/
+
+int main() {
+    Graph<std::string> g;
+    std::vector<int> v;
+    v.push_back(g.add_vertex("undershorts")); // 0
+    v.push_back(g.add_vertex("pants"));       // 1
+    v.push_back(g.add_vertex("belt"));        // 2
+    v.push_back(g.add_vertex("shirt"));       // 3
+    v.push_back(g.add_vertex("tie"));         // 4
+    v.push_back(g.add_vertex("jacket"));      // 5
+    v.push_back(g.add_vertex("sockets"));     // 6
+    v.push_back(g.add_vertex("shoes"));       // 7
+    v.push_back(g.add_vertex("watch"));       // 8
+
+    g.for_each([&](int id, const std::string& value, const std::set<int>& adj) {
+        std::cout << "<" << id << "," << value << ">" << std::endl;
+
+        for (auto id : adj) {
+            std::cout << value << " -> " << g.value(id) << std::endl;
+        }
+    });
+
+    g.add_edge(v[0], v[1]);
+    g.add_edge(v[0], v[7]);
+    g.add_edge(v[1], v[2]);
+    g.add_edge(v[1], v[7]);
+    g.add_edge(v[2], v[5]);
+    g.add_edge(v[3], v[2]);
+    g.add_edge(v[3], v[4]);
+    g.add_edge(v[4], v[5]);
+    g.add_edge(v[6], v[7]);
+
+    std::cout << std::endl;
+    g.for_each([&](int id, const std::string& value, const std::set<int>& adj) {
+        std::cout << "<" << id << "," << value << ">" << std::endl;
+
+        for (auto id : adj) {
+            std::cout << value << " -> " << g.value(id) << std::endl;
+        }
+    });
 }
