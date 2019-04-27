8009856de51249524af9e6c4f656ef24b5a4392a
allen <1007729991@qq.com>
2019-04-27 21:54:14 +0800

    successor & predecessor

--- examples/tree/binary_search_tree_example.cpp
+++ examples/tree/binary_search_tree_example.cpp
@@ -12,6 +12,10 @@ using namespace dsa;
 
 using Tree = BinarySearchTree<int, int>;
 
+std::ostream& operator<<(std::ostream& out, const std::pair<int, int>& val) {
+    return out << "(" << val.first << "," << val.second << ")";
+}
+
 int main() {
     std::vector<std::optional<int>> values = {15, 6, 18, 3, 8, 17, 20, 2, 4, {}, 13, 
             {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, 9};
@@ -42,10 +46,18 @@ int main() {
 
     std::cout << tree_graph << std::endl;
     std::cout << tree << std::endl << std::endl;
+    std::cout << "Find 15: the value is " << tree.search(15).value_or(-1) << std::endl;
+
+    try {
+        std::cout << "15's successor is " << tree.successor(15).value() << std::endl;
+        std::cout << "15's predecessor is " << tree.predecessor(15).value() << std::endl;
+        std::cout << "13's successor is " << tree.successor(13).value() << std::endl;
+        std::cout << "13's predecessor is " << tree.predecessor(13).value() << std::endl << std::endl;
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
 
     tree.left_rotate(15);
     std::cout << tree << std::endl << std::endl;
-    
-    std::cout << "find 15: the value is " << tree.search(15).value_or(-1) << std::endl;
     return 0;
 }
